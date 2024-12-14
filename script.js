c2945b32ab379a8dc34c532d7c58caecff6396d1
Jeet Bhuptani <jeet.work.id@gmail.com>
2024-12-14 11:57:52 +0530

    Portfolio Test

---
+++ script.js
@@ -0,0 +1,10 @@
+function checkOrientation() {
+    const warningMessage = document.querySelector('.warning-message');
+    if (window.innerHeight > window.innerWidth) {
+        warningMessage.classList.remove('d-none');
+    } else {
+        warningMessage.classList.add('d-none');
+    }
+}
+window.addEventListener('resize', checkOrientation);
+window.addEventListener('load', checkOrientation);
