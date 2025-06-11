04124c293e33d8dec253b26c0d94684cac804963
Jeet Bhuptani <122789036+jeetbhuptani@users.noreply.github.com>
2025-06-11 11:36:15 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,11 +24,12 @@ export default function Page() {
                 yOffset={8}
                 text={`Hi, I'm ${DATA.name.split(" ")[0]} 👋`}
               />
-              <BlurFadeText
-                className="max-w-[600px] md:text-xl"
-                delay={BLUR_FADE_DELAY}
-                text={DATA.description}
-              />
+              <BlurFade delay={BLUR_FADE_DELAY}>
+                <div
+                  className="max-w-[600px] md:text-xl text-muted-foreground"
+                  dangerouslySetInnerHTML={{ __html: DATA.description }}
+                />
+              </BlurFade>
             </div>
             <BlurFade delay={BLUR_FADE_DELAY}>
               <Avatar className="size-28 border">
@@ -165,7 +166,7 @@ export default function Page() {
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   During my time in university, I attended{" "}
-                  {DATA.hackathons.length}+ hackathons. People from around the
+                  {DATA.hackathons.length} hackathons. People from around the
                   country would come together and build incredible things in 2-3
                   days. It was eye-opening to see the endless possibilities
                   brought to life by a group of motivated and passionate
