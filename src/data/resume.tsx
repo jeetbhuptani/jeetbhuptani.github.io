2572561da0574fc813dbd689e2ac0639141d77de
Jeet Bhuptani <122789036+jeetbhuptani@users.noreply.github.com>
2025-06-11 11:36:39 +0530

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -17,7 +17,7 @@ export const DATA = {
         age--;
       }
       return age;
-    })()} years old Computer Engineer. I am all about building <s>projects</s> <strong>products<strong>. I love to learn new technologies and apply them to my products.`,
+    })()} years old Computer Engineer. I am all about building <s>projects</s> <strong>products</strong>. I love to learn new technologies and apply them to my products.`,
   summary:
     "I have six years of studying experience with hands on in this field. In the past, [I pursued a diploma in computer engineering](/#education) and [part of 4 hackathons for fun & learning](/#hackathons). I also had the pleasure of being a part of a club [GDG DDU](https://gdg.community.dev/gdg-on-campus-dharmsinh-desai-university-nadiad-india/).",
   avatarUrl: "/jeet.jpg",
